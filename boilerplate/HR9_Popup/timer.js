31f42a6daf2659a1ae8b316bac3f33ac683e0f21
Ryan Huckleberry <ryanthuckleberry@gmail.com>
2019-09-21 13:53:38 -0500

    alrms

--- boilerplate/HR9_Popup/timer.js
+++ boilerplate/HR9_Popup/timer.js
@@ -1,9 +1,11 @@
 function setAlarm(alarm_val) {
     chrome.alarms.create({ delayInMinutes: alarm_val })
     chrome.alarms.onAlarm.addListener(function() {
-        alert("Timer Finished!")
-            //add kill pages code here
+        alert("Timer Finished!");
+        close();
+        //add kill pages code here
     });
 }
+
 
 window.setAlarm = setAlarm
