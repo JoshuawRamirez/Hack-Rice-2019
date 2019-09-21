a9befbf259a392f2c60d408a9c7c87a61a8fd28a
Ryan Huckleberry <ryanthuckleberry@gmail.com>
2019-09-21 12:27:43 -0500

    alarms

--- boilerplate/HR9_Popup/timer.js
+++ boilerplate/HR9_Popup/timer.js
@@ -1,4 +1,6 @@
-chrome.alarms.create({ delayInMinutes: 1.0 })
-chrome.alarms.onAlarm.addListener(function() {
-    alert("Timer Finished!")
-});
+function setAlarm() {
+    chrome.alarms.create({ delayInMinutes: 1.0 })
+    chrome.alarms.onAlarm.addListener(function() {
+        alert("Timer Finished!")
+    });
+}
