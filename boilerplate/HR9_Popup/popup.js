52fce8cdcb4941e07083144ce25dc1846454dfe3
Ananya Vaidya <akv4@rice.edu>
2019-09-21 11:58:42 -0500

    Added localStorage

--- boilerplate/HR9_Popup/popup.js
+++ boilerplate/HR9_Popup/popup.js
@@ -1,5 +1,3 @@
-
-
 document.addEventListener('DOMContentLoaded', function() {
 
     // var sleepButton = document.getElementById('sleepHours');
@@ -7,6 +5,7 @@ document.addEventListener('DOMContentLoaded', function() {
     // var mealsButton = document.getElementById('mealsEaten');
     var submitAll = document.getElementById('submitAll');
     var studyTimer = document.getElementById('studyTimer');
+    var totalSleep = window.localStorage.getItem('sleeptxt');
 
     // sleepButton.addEventListener('click', function() {
     //     window.open('https://hack.rice.edu/');
@@ -24,6 +23,12 @@ document.addEventListener('DOMContentLoaded', function() {
         var sleeptxt = document.getElementById("sleepText").value;
         var studytxt = document.getElementById("studyText").value;
         var mealtxt = document.getElementById("mealText").value;
+
+        window.localStorage.setItem('sleeptxt', totalSleep + sleeptxt);
+        window.localStorage.setItem('studytxt', studytxt);
+        window.localStorage.setItem('mealtxt', mealtxt);
+
+
         if (sleeptxt === "" || studytxt === "" || mealtxt === "") {
             alert("Please fill out the entire form")
         } else {
