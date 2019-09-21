df53ef84093f4968f27eff7d519b5813c01eddcb
Joshua Ramirez <joshuawramirez@gmail.com>
2019-09-20 21:01:18 -0700

    hi

--- boilerplate/HR9_Popup/popup.js
+++ boilerplate/HR9_Popup/popup.js
@@ -1,8 +1,18 @@
 document.addEventListener('DOMContentLoaded', function() {
 
   var sleepButton = document.getElementById('sleepHours');
+  var studyButton = document.getElementById('studyHours');
+  var mealsButton = document.getElementById('mealsEaten');
 
   sleepButton.addEventListener('click', function() {
+     window.open('https://hack.rice.edu/');
+  }, false);
+
+  studyButton.addEventListener('click', function() {
+     window.open('https://hack.rice.edu/');
+ }, false);
+
+  mealsButton.addEventListener('click', function() {
      window.open('https://hack.rice.edu/');
   }, false);
   
