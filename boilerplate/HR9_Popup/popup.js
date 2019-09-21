4fb386f6f2578226cc10ba0cd9d67695f9262666
Ryan Huckleberry <ryanthuckleberry@gmail.com>
2019-09-21 09:02:17 -0500

    added more input alert cases

--- boilerplate/HR9_Popup/popup.js
+++ boilerplate/HR9_Popup/popup.js
@@ -4,6 +4,8 @@ document.addEventListener('DOMContentLoaded', function() {
     // var studyButton = document.getElementById('studyHours');
     // var mealsButton = document.getElementById('mealsEaten');
     var submitAll = document.getElementById('submitAll')
+        //change html text to submit all!!
+
 
     // sleepButton.addEventListener('click', function() {
     //     window.open('https://hack.rice.edu/');
@@ -21,24 +23,35 @@ document.addEventListener('DOMContentLoaded', function() {
         var sleeptxt = document.getElementById("sleepText").value;
         var studytxt = document.getElementById("studyText").value;
         var mealtxt = document.getElementById("mealText").value;
-        if (parseFloat(sleeptxt) < 8) {
-            var message = "Go To SLEEP!!! STOP WORKING, Ryan!!! :'(";
-            alert(message);
+        if (sleeptxt === "" || studytxt === "" || mealtxt === "") {
+            alert("Please fill out the entire form")
         } else {
-            alert("Good job sleeping!!")
-        }
-        if (parseFloat(studytxt) > 5) {
-            var message = "Good job, today! But now it's time to take a break! ^_^";
-            alert(message);
-        } else {
-            //idk about study numbers should we get annoyed if they dont study at all??
-            alert("Good job on your work-life balance today!!")
-        }
-        if (parseFloat(mealtxt) < 3) {
-            var message = "You need to eat! Take care of you self and have some yummy munchies! 8)";
-            alert(message);
-        } else {
-            alert("Good job eating!!")
+            if (parseFloat(sleeptxt) < 8) {
+                var message = "Go To SLEEP!!! STOP WORKING, Ryan!!! :'(";
+                alert(message);
+            } else if ((parseFloat(sleeptxt) >= 8)) {
+                alert("Good job sleeping!!");
+            } else {
+                alert("Please only enter numbers");
+            }
+            if (parseFloat(studytxt) >= 5) {
+                var message = "Good job, today! But now it's time to take a break! ^_^";
+                alert(message);
+            } else if (parseFloat(studytxt) < 1) {
+                alert("Make sure you get you work done!");
+            } else if (parseFloat(studytxt) >= 1 && parseFloat(studytxt) < 5) {
+                alert("Good job on your work-life balance today!!");
+            } else {
+                alert("Please only enter numbers");
+            }
+            if (parseFloat(mealtxt) < 3) {
+                var message = "You need to eat! Take care of you self and have some yummy munchies! 8)";
+                alert(message);
+            } else if ((parseFloat(mealtxt) >= 3)) {
+                alert("Good job eating!!");
+            } else {
+                alert("Please only enter numbers");
+            }
         }
         document.writeln("Data: " + " Sleep Time: " + sleeptxt + " Study Time: \
         " + studytxt + " Meals: " + mealtxt)
